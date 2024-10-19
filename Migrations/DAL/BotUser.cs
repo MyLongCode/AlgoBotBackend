780c6880effcda54bf388bd875ed5af1fc8a4069
mylongcode <mylongcode@gmail.com>
2024-10-19 21:21:07 +0500

    initial project

---
+++ Migrations/DAL/BotUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlgoBotBackend.Migrations.DAL
+{
+    public class BotUser
+    {
+        [Key]
+        public string Username { get; set; }
+        public string? ReferalUsername { get; set; }
+        public string Firstname { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string ChildAge { get; set; } = null!;
+        public string ChildName { get; set; } = null!;
+        public int StageReg { get; set; }
+    }
+}
---
+++ Migrations/EF/DBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AlgoBotBackend.Migrations.DAL;
+using System.ComponentModel;
+using System.Data;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace AlgoBotBackend.Migrations.EF
+{
+    public class DBContext : DbContext
+    {
+
+        public virtual DbSet<BotUser> Users { get; set; } = null!;
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(Console.WriteLine);
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+    }
+}
